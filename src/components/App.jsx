89c251b7f1b10e5ca548e2559d8fd943caee8c1d
Serhii Khabuzov <63494926+Arag0rn@users.noreply.github.com>
2023-10-11 22:36:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 
-import { lazy, Suspense } from "react";
+// import { lazy, Suspense } from "react";
 import {  Routes, Route} from "react-router-dom";
 import { Layout } from "./Layout";
 import { HomePage } from "pages/HomePage";
